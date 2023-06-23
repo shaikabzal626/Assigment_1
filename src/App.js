86351113beb8280599938a55a03a0a02390d06be
shaikabzal626 <118551060+shaikabzal626@users.noreply.github.com>
2023-06-23 18:34:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="Navbar">
+      <ul>
+        <li id="li1">Home</li>
+        <li>News</li>
+        <li>Contact</li>
+        <li id="li4">About</li>
+      </ul>
+    </div>
+  );
+}
